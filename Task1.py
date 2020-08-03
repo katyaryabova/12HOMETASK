0b41445fcc2b0275b3a09c8c9859db1134ca4f43
kateryna <ts-kateryna.riabova@rakuten.com>
2020-08-03 17:31:19 +0300

    Task1

--- Task1.py
+++ Task1.py
@@ -1,11 +1,9 @@
-list1 = [int(s) for s in input("Enter number: ").split()]
-k = int(input("Enter number k: "))
+list1 = [int(s) for s in input().split()]
+k = int(input())
 for i in range(k + 1, len(list1)):
     list1[i - 1] = list1[i]
-    list1.pop()
+list1.pop()
 print(' '.join([str(i) for i in list1]))
-
-
 
 
 
